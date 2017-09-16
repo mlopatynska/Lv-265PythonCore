33e5345596be2aa6051e8b7f685bae52e1a76ca2
IhorMar <rmg1203@gmail.com>
2017-09-16 12:34:49 +0300

    my program mathfigures.py

---
+++ mathfigures.py
@@ -0,0 +1,12 @@
+print "Hello my friend."
+a = input("Input first figure: ")
+b = input("Input second figure: ")
+
+addit = a + b
+substr = a - b
+multipl = a * b
+divis = a / b
+modul = a % b
+
+print("Additional = {}\nSubtraction = {}\nMultiplikation = {}\nDivision = {}\nModule = {}").format(addit, substr,
+                                                                                                  multipl, divis, modul)
