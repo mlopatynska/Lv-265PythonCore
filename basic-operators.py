73ef2536ae7c4e68ef32ed9db95d8e0a6eed6cbd
tarassito <tarassito@gmail.com>
2017-09-19 10:42:14 +0300

    Add HW1 files

---
+++ basic-operators.py
@@ -0,0 +1,19 @@
+a = int(raw_input('Please, enter a '))
+b = int(raw_input(' Enter b '))
+c = 0
+
+c = a+b
+print "Addition: ",c
+
+c = a-b
+print "Subtraction: ",c
+
+c = a*b
+print "Multiplication: ",c
+
+c = a/b
+print "Division: ",c
+
+c = a%b
+print "Modulus: ",c
+
---
+++ raw-input.py
@@ -0,0 +1,5 @@
+name = raw_input('What is your name? ')
+age = raw_input ('What is your age?')
+location = raw_input ('Where are you from?')
+print "Hello, my name is %s. I am %s years old. And I live in %s." % (name, age, location)
+
