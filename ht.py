9985057ed09577ed6eef2dd2adebbfb3fce5744e
Eleshka20 <alyonkakm@gmail.com>
2017-09-19 20:54:38 +0300

    my first commit

---
+++ ht.py
@@ -0,0 +1,16 @@
+﻿name=raw_input("What's your name?")
+print "Shalom,%s " %name
+age=int(raw_input("How old are you?"))
+print "Your age is %s " %age
+location=raw_input("Where are you live?")
+print "You live in %s " %location
+a=int(raw_input("Число а="))
+print a
+b=int(raw_input("Число b="))
+print b
+print a+b
+print a-b
+print a*b
+print a%b
+
+
