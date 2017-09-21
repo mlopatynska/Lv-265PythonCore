3b4c97a6089e0cd28472737269a5c19716ec262b
Yurii Chorneha <ychortc@training.local>
2017-09-21 21:25:53 +0300

    first excersizes

---
+++ ex1.py
@@ -0,0 +1,5 @@
+name = raw_input("What is your name: ")
+age = raw_input("What is your age: ")
+town = raw_input("Where do you live: ")
+
+print "Your name is %s, You are %s years old and You live in %s" % (name, age, town)
--- ex31.py
+++ ex31.py
@@ -7,7 +7,7 @@ x = int(x1)
 y = int(y1)
 z = int(z1)
 
-a = (sqrt(fabs(x-1))-pow(fabs(y),(1/3)))/(1+(x**2)/2+(y**2)/4)
+a = (sqrt(fabs(x-1))-pow(fabs(y),(1/3))) / (1+(x**2)/2+(y**2)/4)
 b = x*(atan(z)+exp(-(x+3)))
 print "a= %s" % (a)
 print "b= %s" % (b)
