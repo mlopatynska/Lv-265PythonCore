de43dae43bdbe4e0d0bf8a7acc22907a7c209783
mithranddir <vadymilyan@gmail.com>
2017-09-22 16:02:36 +0300

    home work. 1.1, 1.2. Done.

---
+++ my home work/task 1.1.py
@@ -0,0 +1,7 @@
+
+name = raw_input (" What is your name? ")
+print ("Hello, " + name + ".")
+age = raw_input (" How old are you? ")
+print ("Your age is " + age + ".")
+home = raw_input (" What city do you live in? ")
+print (" You live in " + home + ".")
---
+++ my home work/task 1.2.py
@@ -0,0 +1,28 @@
+
+num1 = input("Enter the first number: ")
+oper = raw_input("Select operation +,-,*,/ : ")
+num2 = input("Enter the second number: ")
+
+if oper == "+":
+    ans=num1+num2
+    print (ans)
+else:
+    pass
+
+if oper == "-":
+    ans=num1-num2
+    print (ans)
+else:
+    pass
+
+if oper == "*":
+    ans=num1*num2
+    print (ans)
+else:
+    pass
+
+if oper == "/":
+    ans=num1/num2
+    print (ans)
+else:
+    pass
