319d297afb1667f3fb28e3670b4519ec51b73b04
tarassito <tarassito@gmail.com>
2017-09-23 11:09:45 +0300

    Add folders and homework3

---
+++ HW3/HW3-Exchange-variables.py
@@ -0,0 +1,6 @@
+a = 123
+b = 678
+a,b = b,a
+
+print "a is", a
+print "b is", b
---
+++ HW3/HW3-Product-4num.py
@@ -0,0 +1,10 @@
+a = 1234
+b = a % 10
+a = a/10
+c = a % 10
+a = a/10
+d = a % 10
+a = a/10
+e = a % 10
+print(a)
+print(b*c*d*e)
