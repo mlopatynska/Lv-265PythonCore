68eff48c21b716eab74efe296f74d8c9258405b0
Mytsa <tomoemulo1@gmail.com>
2017-09-25 23:26:32 +0300

    move files to HW-3

---
+++ HW-3/HW-3(change numbers).py
@@ -0,0 +1,6 @@
+a = float(raw_input("number a: "))
+b = float(raw_input("number b: "))
+
+a, b = b, a
+
+print ("a now is: {}, \nb now is: {}".format(a, b))
---
+++ HW-3/HW-3(numbers_operation).py
@@ -0,0 +1,7 @@
+l = '2341'
+
+sort = sorted(l)
+print("sort of numbers: {}".format(sort))
+print ("revers of sort: {}".format(sort[::-1]))
+res = int(l[0])*int(l[1])*int(l[2])*int(l[3])
+print ("mnozenna: {}".format(res))
