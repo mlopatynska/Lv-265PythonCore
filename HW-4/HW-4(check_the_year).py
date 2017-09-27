09a57a964d16435c2ed1d33633df0001fe41512f
Mytsa <tomoemulo1@gmail.com>
2017-09-27 21:23:48 +0300

    file with correct date

---
+++ HW-4/HW-4(check_the_year).py
@@ -0,0 +1,30 @@
+import datetime
+
+day = int(raw_input("day: "))
+month = int(raw_input("month: "))
+year = int(raw_input("year: "))
+if month < 13:
+    if month % 2 == 0:
+        if month == 2:
+            if year % 4 == 0 and day < 30:
+                print "date is correct"
+            elif day < 29:
+                print "date is correct"
+            else:
+                print "date is NOT correct"
+        elif month == 8:
+            if day < 32:
+                print "date is correct"
+            else:
+                print "date is NOT correct"
+        elif day < 31:
+            print "date is correct"
+        else:
+            print "date is NOT correct"
+    else:
+        if day < 32:
+            print "date is correct"
+        else:
+            print "date is NOT correct"
+else:
+    print "date is NOT correct"
