339571b157a67807493383d604f9414b87a4bfb7
Yuriy Chorneha <yuch17@gmail.com>
2017-09-28 22:45:34 +0300

    fixed

--- ex52.py
+++ ex52.py
@@ -6,7 +6,7 @@ if day > 31:
     print "Date is NOT CORRECT"
 elif month < 1 or month > 12:
     print "Date is NOT CORRECT"
-    elif (((month == 2)or (month == 4) or (month == 6) or (month == 9) or (month == 11)) and (day == 31)) or (month == 2 and day == 30) or ((year % 4 == 0 and month == 2 and day == 29)):
+    elif (((month == 2)or (month == 4) or (month == 6) or (month == 9) or (month == 11)) and (day == 31)) or (month == 2 and day == 30) or ((year % 4 != 0 and month == 2 and day == 29)):
         print "Date is NOT CORRECT"
 else:
     print "Date is CORRECT!"
