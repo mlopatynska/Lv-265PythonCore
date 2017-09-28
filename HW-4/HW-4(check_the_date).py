4a1f33e305bd7539c571a200a31999dd01162eb4
Viktor <31900832+Mytsa@users.noreply.github.com>
2017-09-28 10:43:01 +0300

    mistake, must be 0<month<13. I will correct later.

--- HW-4/HW-4(check_the_date).py
+++ HW-4/HW-4(check_the_date).py
@@ -1,7 +1,7 @@
 day = int(raw_input("day: "))
 month = int(raw_input("month: "))
 year = int(raw_input("year: "))
-if month < 13:
+if month < 13: # mistake with 0 month
     if month % 2 == 0:
         if month == 2:
             if year % 4 == 0 and day < 30:
