2005f69404595f67b23f3439737b2a7ce5ecce9c
Yuriy Chorneha <yuch17@gmail.com>
2017-10-01 19:08:57 +0300

    6 TEST prime number

---
+++ ex62.py
@@ -0,0 +1,14 @@
+a = int(raw_input("Enter A:"))
+
+x = True 
+for k in range(2, a-1):
+while x:
+if a%i == 0:
+x = False
+else:
+x = True
+
+if x:
+print ("{} is a PRIME number").format(a)
+else:
+print ("{} is a COMPOSITE number").format(a)
