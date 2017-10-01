98b46a7e0f1d0f35b8c232f24306dba1a12fffc4
Yuriy Chorneha <yuch17@gmail.com>
2017-10-01 22:02:41 +0300

    6 TEST sequence in text

---
+++ ex65.py
@@ -0,0 +1,24 @@
+text = raw_input("Enter text:")
+seq = raw_input("Enter sequence of symbols:")
+
+if len(text)<len(seq):
+print ("Sorry, this sequence is longer than entire text :)")
+elif len(text)=len(seq):
+check = True
+for i in range(len(seq)):
+if text[i]!=seq[i]):
+check = False
+if check: s = 1
+else: s = 0
+
+else:
+for j in range(len(text)-len(seq)):
+if text[j] = " ":
+check = True
+for k in range(j+1, j+len(seq)):
+if text[k]!=seq[k-j]):
+check = False
+if check: s = s+1
+
+
+print ("In the text "{}" sequence of symbols "{}" happens {} times).format(text, seq, s)
