46ab11796dd4dba965dba8f23491d7d3539d1e3d
Yuriy Chorneha <yuch17@gmail.com>
2017-10-01 22:29:34 +0300

    seems S ang y go different ways for some x

---
+++ ex661.py
@@ -0,0 +1,22 @@
+from math import abs
+
+x = float(raw_input("Enter x:"))
+
+y = 2*x/((1-x)**3)
+print ("y= {}").format(y)
+
+k = 1
+s = 0
+while krok<(10**(-5)):
+krok = k*(k+1)*(x**k)
+s = s + krok
+k = k+1
+
+else:
+print ("S= {}").format(s)
+
+Pohybka = abs((s-y)/y)*100
+print ("Pohybka= {}").format(Pohybka)
+
+
+
