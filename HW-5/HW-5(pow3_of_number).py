6bd258bfd2f34a0b6ed49681465dcd6040d61f88
Mytsa <tomoemulo1@gmail.com>
2017-10-01 22:33:20 +0300

    add file with sum of cubes

---
+++ HW-5/HW-5(pow3_of_number).py
@@ -0,0 +1,11 @@
+num = raw_input("input number: ")
+
+n = 0
+res1 = 0
+rg = int(len(num))
+
+while n < rg:
+    res = int(num[n])
+    res1 = res1 + (res**3)
+    n += 1
+print "sum cubes of number {} is: {}".format(num ,res1)
