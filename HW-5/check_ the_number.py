3e8ee600d631020d1bdc4b1383e79129247e9670
Mytsa <tomoemulo1@gmail.com>
2017-10-01 21:19:58 +0300

    add file for check prime numbers

---
+++ HW-5/check_ the_number.py
@@ -0,0 +1,9 @@
+num = int(raw_input("input number: "))
+
+i = 2
+for i in range(2, num):
+    if num % i == 0:
+        print("you input NOT prime number")
+    else:
+        print("you input prime number")
+    break
