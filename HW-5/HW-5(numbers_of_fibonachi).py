8edcf4b35779270a5ec131d5ece9a4489871ebd0
Mytsa <tomoemulo1@gmail.com>
2017-10-02 19:55:22 +0300

    reduce code

--- HW-5/HW-5(numbers_of_fibonachi).py
+++ HW-5/HW-5(numbers_of_fibonachi).py
@@ -1,11 +1,7 @@
 num = int(raw_input("input numbers: "))
 
-a = 0
-b = 1
+a, b = 0, 1
 
 for i in range(2, num + 1):
-    c = a + b
-    a, b = b, c
-    print c
-
-
+    print a
+    a, b = b, a + b
