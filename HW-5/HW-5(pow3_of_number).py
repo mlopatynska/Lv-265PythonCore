01283155bd3a1da6ffc968c4df5aae51c8e8e44d
Mytsa <tomoemulo1@gmail.com>
2017-10-02 19:59:32 +0300

    reduce code

--- HW-5/HW-5(pow3_of_number).py
+++ HW-5/HW-5(pow3_of_number).py
@@ -1,11 +1,10 @@
 num = raw_input("input number: ")
 
 n = 0
-res1 = 0
+res = 0
 rg = int(len(num))
 
 while n < rg:
-    res = int(num[n])
-    res1 = res1 + (res**3)
+    res = res + (int(num[n])**3)
     n += 1
-print "sum cubes of number {} is: {}".format(num, res1)
+print "sum cubes of number {} is: {}".format(num, res)
