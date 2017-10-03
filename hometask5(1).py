751ad716ad3693978e753dadded5d652e7f40aca
Eleshka20 <alyonkakm@gmail.com>
2017-10-03 16:32:38 +0300

    Hometask 5.1

---
+++ hometask5(1).py
@@ -0,0 +1,10 @@
+﻿Sum=input('Задане число n:')
+for i in range(1000,9999):
+    num=i
+    sum=0
+    while num>0 :
+        last=num%10
+        num=num/10
+        sum=sum+last
+    if sum==Sum:
+        print ("Нашій сумі відповідають наступні числа:", i)
