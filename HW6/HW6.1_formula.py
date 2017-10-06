7be8989c8dc24afe2cbf28115bfdc53c336b816d
tarassito <tarassito@gmail.com>
2017-10-06 16:40:21 +0300

    Add HW6.1 formula

---
+++ HW6/HW6.1_formula.py
@@ -0,0 +1,15 @@
+def list(parameter, variable):
+    s=0
+    for i in range(3):
+        s = s + parameter*(variable**(2-i))
+    return s
+
+a = float(raw_input("Enter a: "))
+x = float(raw_input("Enter x: "))
+b = float(raw_input("Enter b: "))
+y = float(raw_input("Enter y: "))
+c = float(raw_input("Enter c: "))
+z = float(raw_input("Enter z: "))
+
+result = (((list(a,x))**2)-list(b,y))/list(c,(x+z))
+print result
