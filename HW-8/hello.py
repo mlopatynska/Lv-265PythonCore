ed0088e990d958b245e622e006d918c4e423e956
Mytsa <tomoemulo1@gmail.com>
2017-10-23 19:41:03 +0300

    add information on page and photo

--- HW-8/hello.py
+++ HW-8/hello.py
@@ -2,18 +2,34 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
     return render_template("Hello_world.html")
 
+
 @app.route('/login/')
-@app.route('/login/<username>')
+@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/<name>')
 def login(username=None):
     return render_template("login.html", username=username)
+
 
 @app.route('/about/')
 @app.route('/about/<name>')
 def about(username=None):
     return render_template("about.html", username=username)
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
