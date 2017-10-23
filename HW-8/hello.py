7a5ce8a1af2b18c693c9ba50a5ff7fab8aaa9a79
Mytsa <tomoemulo1@gmail.com>
2017-10-23 22:06:59 +0300

    correct css file

--- HW-8/hello.py
+++ HW-8/hello.py
@@ -1,17 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 
 def home():
+    if 'username' in session:
+            return 'Logged in as %s' % escape(session['username'])
     return render_template("Hello_world.html")
 
 
-@app.route('/login/')
-# @app.route('/login', methods=['GET', 'POST'])
-@app.route('/login/<name>')
-def login(username=None):
-    return render_template("login.html", username=username)
+
+# @app.route('/login/')
+# @app.route('/login/<name>')
+# def login(username=None):
+#     return render_template("login.html", username=username)
+
+# @app.route('/')
+# def index():
+#     if 'username' in session:
+#         return 'Logged in as %s' % escape(session['username'])
+#     return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('Hello_world.html'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('Hello_world.html'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'aaa'
+
+
 
 
 @app.route('/about/')
@@ -20,11 +51,7 @@ def about(username=None):
     return render_template("about.html", username=username)
 
 
-# @app.route('/')
-# def index():
-#     if 'username' in session:
-#         return 'Logged in as %s' % escape(session['username'])
-#     return 'You are not logged in'
+
 
 
 
