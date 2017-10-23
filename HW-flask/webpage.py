037d8e347b5a4103c054c353915e3e5debdb896f
tarassito <tarassito@gmail.com>
2017-10-23 22:56:57 +0300

    Add Flask homework

--- HW-flask/webpage.py
+++ HW-flask/webpage.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
-def index ():
+def main ():
     return render_template("index.html")
+
+@app.route("/about")
+def about ():
+    return render_template("about.html")
+
+@app.route("/skills")
+def skills ():
+    skills = ["HTML", "CSS", "JavaScript", "Python", "JQuery"]
+    return render_template("skills.html", skills = skills)
+
+@app.route("/pygame")
+def pygame ():
+    return render_template("pygame.html")
 
 if __name__  == "__main__":
     app.run(debug=True)
